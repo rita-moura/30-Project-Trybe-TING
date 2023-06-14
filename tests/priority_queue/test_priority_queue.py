624e776f0594f09337f00a174d18ca3ef1a60b8a
Rita Moura <ritamoura520@gmail.com>
2023-06-14 10:29:36 -0300

    Feat: implemneta testes para classe PriorityQueue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,24 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queueing = PriorityQueue()
+
+    priority_queueing.enqueue({"qtd_linhas": 2})
+    priority_queueing.enqueue({"qtd_linhas": 10})
+    priority_queueing.enqueue({"qtd_linhas": 3})
+    priority_queueing.enqueue({"qtd_linhas": 6})
+
+    assert len(priority_queueing) == 4
+    assert priority_queueing.search(1) == {"qtd_linhas": 3}
+
+    given = priority_queueing.dequeue()
+    assert len(priority_queueing) == 3
+    assert given == {"qtd_linhas": 2}
+
+    given = priority_queueing.dequeue()
+    assert len(priority_queueing) == 2
+    assert given == {"qtd_linhas": 3}
+    with pytest.raises(IndexError):
+        priority_queueing.search(10)
