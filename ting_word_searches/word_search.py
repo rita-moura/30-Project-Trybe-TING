dd37049007c44107b7c499af3fb12ce13590c3ab
Rita Moura <ritamoura520@gmail.com>
2023-06-14 10:42:01 -0300

    Feat: implementa a função search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -28,4 +28,30 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    counter = 0
+    result = []
+
+    for i in range(len(instance)):
+        fila = instance.search(i)
+        
+        instance_result = {
+            "palavra": word,
+            "arquivo": fila["nome_do_arquivo"],
+            "ocorrencias": [
+                {
+                    "linha": index + 1,
+                    "conteudo": linha
+                } for index, linha in enumerate(fila["linhas_do_arquivo"])
+                if word.lower() in linha.lower()
+            ]
+        }
+        
+        result.append(instance_result)
+        
+        if len(instance_result["ocorrencias"]) == 0:
+            counter += 1
+
+    if counter == len(instance):
+        return []
+    
+    return result
