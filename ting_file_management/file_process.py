d5c8676a10e9279a46494343ecbef75086cc279b
Rita Moura <ritamoura520@gmail.com>
2023-06-14 10:03:46 -0300

    Feat: implementa a função process.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        result = instance.search(i)
+        if result["nome_do_arquivo"] == path_file:
+            return
+
+    content = txt_importer(path_file)
+    dict_to_return = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(content),
+        "linhas_do_arquivo": content
+        }
+    instance.enqueue(dict_to_return)
+    print(dict_to_return)
 
 
 def remove(instance):
